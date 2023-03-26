aee276be3ff397fd72fbc8e12fd632be2e33a4ff
Joseph Murray <josephwmurray@gmail.com>
2023-03-25 20:53:20 -0700

    establish API_KEY

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Template from '.js/template.js';
+
+function getRates () {
+  const url = `https://v6.exchangerate-api.com/v6/{process.env.API_KEY}/latest/{nation}`
+}
